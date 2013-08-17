4976a9d7f6e23ef72e0c7ea3ddbc0c9a39e41b10
Rob Tillaart <rob.tillaart@gmail.com>
2013-08-17 16:24:01 +0200

    + version 0.1.03 + added resolution

--- libraries/StopWatch/StopWatch.cpp
+++ libraries/StopWatch/StopWatch.cpp
@@ -1,67 +1,83 @@
 // 
 //    FILE: StopWatch.cpp
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.1.02
+// VERSION: 0.1.03
 // PURPOSE: Simple StopWatch library for Arduino
 //
-// The library is based upon millis() and therefor has the same restrictions as millis() has wrt overflow.
+// The library is based upon millis() and therefore
+// has the same restrictions as millis() has wrt overflow.
 //
 // HISTORY: 
 // 0.1.00 - 2011-01-04 initial version
 // 0.1.01 - 2011-01-04 Added better state
 // 0.1.02 - 2011-06-15 Added state() + #defines + lib version
+// 0.1.03 - 2012-01-22 Added several improvements
+//             By mromani & Rob Tillaart
 // 
 // Released to the public domain
 //
 
 #include "StopWatch.h"
-#include "wiring.h"
 
-StopWatch::StopWatch()
+StopWatch::StopWatch(enum Resolution res)
 {
-	reset();
+    _res = res;
+    switch(_res) {
+        case MICROS:
+            _gettime = micros;
+            break;
+        case MILLIS:
+            _gettime = millis;
+            break;
+        case SECONDS:
+            _gettime = seconds;
+            break;
+        default:  
+            _gettime = millis;
+            break;
+    }
+    reset();
 }
 
 void StopWatch::reset()
 {
-	_state = STOPWATCH_RESET;
-	_starttime = _stoptime = 0;
+    _state = StopWatch::RESET;
+    _starttime = _stoptime = 0;
 }
 
 void StopWatch::start()
 {
-	if (STOPWATCH_RESET == _state || STOPWATCH_STOPPED == _state)
-	{
-		_state = STOPWATCH_RUNNING;
-		unsigned long t = millis();
-		_starttime += t - _stoptime;
-		_stoptime = t;
-	}
+    if (_state == StopWatch::RESET || _state == StopWatch::STOPPED)
+    {
+        _state = StopWatch::RUNNING;
+        unsigned long t = _gettime();
+        _starttime += t - _stoptime;
+        _stoptime = t;
+    }
 }
 
 unsigned long StopWatch::value()
 {
-	if (STOPWATCH_RUNNING == _state) _stoptime = millis();
-	return _stoptime - _starttime;
+    if (_state == StopWatch::RUNNING) _stoptime = _gettime();
+    return _stoptime - _starttime;
 }
 
 void StopWatch::stop()
 {
-	if (STOPWATCH_RUNNING == _state)
-	{
-		_state = STOPWATCH_STOPPED;
-		_stoptime = millis();
-	}
+    if (_state == StopWatch::RUNNING)
+    {
+        _state = StopWatch::STOPPED;
+        _stoptime = _gettime();
+    }
 }
 
 bool StopWatch::isRunning()
 {
-	return (STOPWATCH_RUNNING == _state);
+    return (_state == StopWatch::RUNNING);
 }
 
-int StopWatch::state()
+enum StopWatch::State StopWatch::state()
 {
-	return _state;
+    return _state;
 }
-
 // END OF FILE

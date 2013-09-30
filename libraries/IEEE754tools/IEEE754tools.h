4cf986a132bb2d42c3a7be36c5dfbc10433305a9
Rob Tillaart <rob.tillaart@gmail.com>
2013-09-30 17:17:46 +0200

    + added SHIFT_POW2

--- libraries/IEEE754tools/IEEE754tools.h
+++ libraries/IEEE754tools/IEEE754tools.h
@@ -1,7 +1,7 @@
 //
 //    FILE: IEEE754tools.h
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.1.01
+// VERSION: 0.1.02
 // PURPOSE: IEEE754 tools
 //
 // http://playground.arduino.cc//Main/IEEE754tools
@@ -9,6 +9,7 @@
 // Released to the public domain
 // not tested, use with care
 //
+// 0.1.02 added SHIFT_POW2
 // 0.1.01 added IEEE_NAN, IEEE_INF tests + version string
 // 0.1.00 initial version
 
@@ -22,7 +23,7 @@
 #include "WProgram.h"
 #endif
 
-#define IEEE754_VERSION "0.1.01"
+#define IEEE754_VERSION "0.1.02"
 
 // (un)comment lines to configure functionality / size
 //#define IEEE754_ENABLE_MSB   // +78 bytes
@@ -203,8 +204,23 @@ bool IEEE_NegINF(float number)
     return (* ((uint16_t*) &number + 1) ) == 0xFF80; 
 }
 
+// factor 2.7; but more correct
+float IEEE_POW2(float number, int n)
+{
+    _FLOATCONV fl;
+    fl.f = number;
+    int e = fl.p.e + n;
+    if (e >= 0 && e < 256)
+    {
+        fl.p.e = e;
+        return fl.f;
+    }
+    return fl.p.s * INFINITY;
+}
+
+
 // WARNING no overflow detection in the SHIFT (factor 3.5)
-float IEEE_SHIFT_LEFT(float number, uint8_t n)
+float IEEE_POW2fast(float number, int n)
 {
     _FLOATCONV fl;
     fl.f = number;
@@ -212,19 +228,20 @@ float IEEE_SHIFT_LEFT(float number, uint8_t n)
     return fl.f;
 }
 
-float IEEE_SHIFT_RIGHT(float number, uint8_t n)
-{
-    _FLOATCONV fl;
-    fl.f = number;
-    fl.p.e -= n;
-    return fl.f;
-}
-
-
 
 //
 // NOT TESTED FUNCTIONS
 //
+// what is this???
+float IEEE_FLIP(float number)
+{
+    _FLOATCONV fl;
+    fl.f = number;
+    fl.p.e = -fl.p.e;
+    fl.p.m = (0x007FFFFF - fl.p.m);
+    return fl.f;
+}
+
 uint8_t getSign(float number)
 {
   IEEEfloat* x = (IEEEfloat*) ((void*)&number);

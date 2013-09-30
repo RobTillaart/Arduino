3c55123868e4aacc4a4a47dcc7313050050771a2
Rob Tillaart <rob.tillaart@gmail.com>
2013-09-30 18:31:40 +0200

    + static PulsePatternOut PPO for short + refactor & fixing a lot

--- libraries/PulsePattern/PulsePatternOut.cpp
+++ libraries/PulsePattern/PulsePatternOut.cpp
@@ -6,43 +6,53 @@
 //
 // HISTORY:
 // 0.0.1 - 2012-11-23 initial version
+// 0.0.2 - 2012-11-23 adapted a static PPO
 //
 // Released to the public domain
 //
+// TODO 
+// - PRE 1.0 backwards compatibility
+// - move code to .h file so compiler can inline?
+// - optimize timer code
+// - adjust timing to more accurate values -> setTimer()
+//
 
 #include "PulsePatternOut.h"
-// TODO PRE 1.0 backwards compat
+
 #include "Arduino.h"
 
 enum { NO_CLOCK, PRESCALE_1, PRESCALE_8, PRESCALE_64, PRESCALE_256, PRESCALE_1024 };
 
-PulsePatternOut::PulsePatternOut(uint8_t pin)
+PulsePatternOut PPO;
+
+ISR(TIMER1_COMPA_vect)
 {
-	_pin = pin;
-	pinMode(_pin, OUTPUT);
-	digitalWrite(_pin, LOW);
+  PPO::worker();
+}
+
+PulsePatternOut::PulsePatternOut()
+{
 	_size = 0;
-	_cnt = 0;
-	_state = STOPPED;
+	_state = NOTINIT;
 }
 
-PulsePatternOut::~PulsePatternOut()
-{
-}
-
-void PulsePatternOut::init(uint16_t * ar, uint8_t size, uint8_t level = LOW)
+void PulsePatternOut::init(uint8_t pin, uint16_t * ar, uint8_t size, uint8_t level)
 { 
+	stop();
+	_pin = pin;
 	_ar = ar;
 	_size = size;
 	_level = level;
 	_cnt = 0;
-	_state = STOPPED;
+
+	pinMode(_pin, OUTPUT);
+	digitalWrite(_pin, _level);
 }
 
 void PulsePatternOut::start()
 {
-	if (_size == 0) return;
-	if (_state == RUNNING) return;
+	if (_size == 0) return;  
+	if (_state == RUNNING) return;  // no restart
 	setTimer(1);  // start asap
 	_state = RUNNING;
 }
@@ -51,6 +61,8 @@ void PulsePatternOut::stop()
 {
 	stopTimer();
 	_state = STOPPED;
+	_level = LOW;
+	digitalWrite(_pin, _level);
 }
 
 bool PulsePatternOut::isRunning()
@@ -60,39 +72,39 @@ bool PulsePatternOut::isRunning()
 
 void PulsePatternOut::worker()
 {
-	if (_cnt == _size || _state == STOPPED) 
-	{
-		_state = STOPPED;
-		_cnt = 0;
-		stopTimer();
-		return;
-	}
-
+	if (_state != RUNNING) return;
+	// set next period & flip signal
 	_level = !_level;
-	// improve - absorb digitalwrite details here
 	digitalWrite(_pin, _level);
-
-	setTimer(_ar[_cnt]);
+	// TODO small adjustment needed for code overhead?; inline iso call?
+	// for now
+	// + 5.2 usec for digitalWrite
+	// + 3 usec for settimer call
+	setTimer(_ar[_cnt]-8);
+	
 	_cnt++;
+	if (_cnt >= _size) _cnt = 0;  // repeat
 }
 
 // TIMER code based upon - http://www.gammon.com.au/forum/?id=11504
-
 void PulsePatternOut::stopTimer()
 {
 	TCCR1A = 0;        // reset timer 1
 	TCCR1B = 0;
 }
 
+// TODO: can be optimized?
 void PulsePatternOut::setTimer(uint16_t cc)
 {
-	TCCR1A = 0;			// reset timer 1
+	TCCR1A = 0;
 	TCCR1B = 0;
 	TCNT1 = 0;      	// reset counter
-    OCR1A = cc*2;		// compare A register value; 
+    OCR1A = cc*2;		// compare A register value;  
+						// *2 because of 8 prescaler @16mhz;
+	
 	// 4: CTC mode, top = OCR1A
 	TCCR1A = _BV (COM1A1);  	// clear on compare
-	TCCR1B = _BV (WGM12) | PRESCALE_1024;  // should be PRESCALE_8
+	TCCR1B = _BV (WGM12) | PRESCALE_1024;  // should be PRESCALE_8 now for testing
 	TIFR1 |= _BV (OCF1A);    // clear interrupt flag
 	TIMSK1 = _BV (OCIE1A);   // interrupt on Compare A Match   
 }
--- libraries/PulsePattern/PulsePatternOut.h
+++ libraries/PulsePattern/PulsePatternOut.h
@@ -4,7 +4,7 @@
 //    FILE: PulsePatternOut.h
 //  AUTHOR: Rob dot Tillaart at gmail dot com  
 // PURPOSE: PulsePatternOut library for Arduino
-//          sends out a pulse pattern over a digital pin
+//          sends a pulse pattern to a digital pin (continuously)
 // HISTORY: See PulsePatternOut.cpp
 //
 // Released to the public domain
@@ -12,25 +12,25 @@
 
 #include <inttypes.h>
 
-#define PULSEPATTERNOUT_LIB_VERSION "0.0.1"
+#define PULSEPATTERNOUT_LIB_VERSION "0.0.2"
 
+#define NOTINIT -1
 #define STOPPED 0
 #define RUNNING 1
-
 
 class PulsePatternOut 
 {
 	public:
-	PulsePatternOut(uint8_t pin);
-	~PulsePatternOut();
-	void init(uint16_t * ar, uint8_t size, uint8_t level);
+	PulsePatternOut();
+	
+	void init(uint8_t pin, uint16_t * ar, uint8_t size, uint8_t level);
 	void start();
 	void stop();
 	bool isRunning();
+	
+private:
 	void worker();
 	
-protected:
-
 	void stopTimer();
 	void setTimer(uint16_t cc);
 	
@@ -38,9 +38,11 @@ protected:
 	uint8_t _size;
 	uint8_t _pin;
 	volatile uint8_t _level;
-	volatile uint8_t _state;
+	volatile int8_t _state;
 	volatile uint8_t _cnt;
 };
+
+static PulsePatternOut PPO;
 
 #endif
 // END OF FILE

fcd94987b07591f5d0710a716e1b530874403f76
Rob Tillaart <rob.tillaart@gmail.com>
2013-11-09 16:55:47 +0100

    + version 0.1.02 + refactoring

--- libraries/AD524X/AD524X.h
+++ libraries/AD524X/AD524X.h
@@ -1,7 +1,7 @@
 //
 //    FILE: AD524X.h
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.1.01
+// VERSION: 0.1.02
 // PURPOSE: I2C digital PotentioMeter AD5241 AD5242
 //    DATE: 2013-10-12
 //     URL:
@@ -20,7 +20,7 @@
 
 #include <Wire.h>
 
-#define AD524X_VERSION "0.1.01"
+#define AD524X_VERSION "0.1.02"
 
 #define AS524X_OK       0
 #define AS524X_ERROR    -10
@@ -28,24 +28,19 @@
 class AD524X
 {
 public:
-    // address
-    AD524X(uint8_t);
+    AD524X(uint8_t address);
     
     uint8_t zeroAll();
-    // rdac
-    uint8_t read(uint8_t);
-    // rdac value
-    uint8_t write(uint8_t, uint8_t);
-    // rdac value O1 O2
-    uint8_t write(uint8_t, uint8_t, uint8_t, uint8_t);
+    uint8_t read(uint8_t rdac);
+    uint8_t write(uint8_t rdac, uint8_t value);
+    uint8_t write(uint8_t rdac, uint8_t value, uint8_t O1, uint8_t O2);
     
-    uint8_t setO1(uint8_t);  // HIGH / LOW
-    uint8_t setO2(uint8_t);  // HIGH / LOW
+    uint8_t setO1(uint8_t value);  // HIGH / LOW
+    uint8_t setO2(uint8_t value);  // HIGH / LOW
     uint8_t getO1();
     uint8_t getO2();
-    
-    // rdac
-    uint8_t midScaleReset(uint8_t);
+
+    uint8_t midScaleReset(uint8_t rdac);
     //
     // uint8_t shutDown();
 

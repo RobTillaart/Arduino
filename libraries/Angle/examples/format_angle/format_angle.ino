c205a7f5c50b2cdd2d527428f5114d823eca5f40
Chris--A <chris@genx.biz>
2015-08-03 22:02:15 +1000

    Added example format_angle to Angle library.﻿

---
+++ libraries/Angle/examples/format_angle/format_angle.ino
@@ -0,0 +1,24 @@
+/*
+ * Printing a formatted Angle example.
+ * 
+ * Written by: Christopher Andrews (https://github.com/Chris--A)
+ * 
+ * Licenced under MIT, free to use, blah blah...
+ */
+
+#include <Angle.h>
+
+void setup() {
+  Serial.begin(9600);
+
+  Angle angle( 23, 12, 10, 123 );
+
+  Serial.println( angle );                  // Print the most verbose version ( same as angle.format(T) )
+  Serial.println( "-------------------" );
+  Serial.println( angle.format(D) );        // Print only degree value.
+  Serial.println( angle.format(M) );        // Output includes minutes.
+  Serial.println( angle.format(S) );        // Output includes minutes, seconds.
+  Serial.println( angle.format(T) );        // Output includes minutes, seconds, thousands.
+}
+
+void loop() {}

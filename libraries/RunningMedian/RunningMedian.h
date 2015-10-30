6a5296d3f19e21a7e6e8de92753594cabca43415
rob tillaart <rob.tillaart@gmail.com>
2015-10-30 16:46:22 +0100

    + refactor constructor + const where possible

--- libraries/RunningMedian/RunningMedian.h
+++ libraries/RunningMedian/RunningMedian.h
@@ -2,7 +2,7 @@
 //    FILE: RunningMedian.h
 //  AUTHOR: Rob dot Tillaart at gmail dot com
 // PURPOSE: RunningMedian library for Arduino
-// VERSION: 0.1.11 
+// VERSION: 0.1.12
 //     URL: http://arduino.cc/playground/Main/RunningMedian
 // HISTORY: See RunningMedian.cpp
 //
@@ -20,7 +20,7 @@
 
 #include <inttypes.h>
 
-#define RUNNING_MEDIAN_VERSION "0.1.11"
+#define RUNNING_MEDIAN_VERSION "0.1.12"
 
 // prepare for dynamic version
 // not tested use at own risk :)
@@ -33,7 +33,7 @@
 
 // should at least be 5 to be practical
 // odd size results in a 'real' middle element.
-// even size takes the lower of the two middle elements (TODO)
+// even size takes the lower of the two middle elements
 #define MEDIAN_MIN_SIZE     1
 #define MEDIAN_MAX_SIZE     19          // adjust if needed
 
@@ -41,25 +41,25 @@
 class RunningMedian
 {
 public:
-    RunningMedian(uint8_t size);        // # elements in the internal buffer
-    ~RunningMedian();                   // destructor
+    explicit RunningMedian(const uint8_t size);  // # elements in the internal buffer
+    ~RunningMedian();                            // destructor
 
-    void clear();                       // resets internal buffer and var
-    void add(double value);              // adds a new value to internal buffer, optionally replacing the oldest element.
+    void clear();                        // resets internal buffer and var
+    void add(const double value);        // adds a new value to internal buffer, optionally replacing the oldest element.
     double getMedian();                  // returns the median == middle element
 
 #ifdef RUNNING_MEDIAN_ALL
-    double getAverage();                 // returns average of the values in the internal buffer
+    double getAverage();           // returns average of the values in the internal buffer
     double getAverage(uint8_t nMedian);  // returns average of the middle nMedian values, removes noise from outliers
-    double getHighest();                 // returns highest element
-    double getLowest();                  // return lowest element
+    double getHighest();           // returns highest element
+    double getLowest();            // return lowest element
 
-    double getElement(uint8_t n);        // get n'th element from the values in time order
-    double getSortedElement(uint8_t n);  // get n'th element from the values in size order
-    double predict(uint8_t n);           // predict the max change of median after n additions
+    double getElement(const uint8_t n);        // get n'th element from the values in time order
+    double getSortedElement(const uint8_t n);  // get n'th element from the values in size order
+    double predict(const uint8_t n);           // predict the max change of median after n additions
 
-    uint8_t getSize()  { return _size; };  // returns size of internal buffer
-    uint8_t getCount() { return _cnt; };   // returns current used elements, getCount() <= getSize()
+    uint8_t getSize() { return _size; };  // returns size of internal buffer
+    uint8_t getCount() { return _cnt; };  // returns current used elements, getCount() <= getSize()
 #endif
 
 protected:

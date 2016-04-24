f54a04408a41da0fd9a6e8b57c5dad660d4e9afd
rob tillaart <rob.tillaart@gmail.com>
2016-04-24 08:26:22 +0200

    + version 0.1.00 BETA + not tested yet

---
+++ libraries/PCA9635/PCA9635.cpp
@@ -0,0 +1,132 @@
+//
+//    FILE: PCA9635.cpp
+//  AUTHOR: Rob Tillaart
+//    DATE: 23-apr-2016
+// VERSION: 0.1.00
+// PURPOSE: I2C PCA9635 library for Arduino
+//     URL:
+//
+// HISTORY:
+// 0.1.00 initial BETA version
+//
+
+#include "PCA9635.h"
+
+#include <Wire.h>
+
+PCA9635::PCA9635(const uint8_t deviceAddress)
+{
+    _address = deviceAddress;
+    Wire.begin();
+    // TWBR = 12; // 400KHz
+    _data = 0;
+    _error = 0;
+}
+
+// write value to single PWM registers
+void PCA9635::write1(uint8_t channel, uint8_t value)
+{
+    writeN(channel, &value, 1);
+}
+
+// write three values in consecutive PWM registers
+// typically for RGB values
+void PCA9635::write3(uint8_t channel, uint8_t R, uint8_t G, uint8_t B)
+{
+    uint8_t arr[3] = { R, G, B };
+    writeN(channel, arr, 3);
+}
+
+// write count values in consecutive PWM registers
+void PCA9635::writeN(uint8_t channel, uint8_t* arr, uint8_t count)
+{
+    uint8_t base = PCA9635_PWM(channel);
+    Wire.beginTransmission(_address);
+    Wire.write(base);
+    for(uint8_t i = 0; i < count; i++)
+    {
+        Wire.write(arr[i]);
+    }
+    _error = Wire.endTransmission();
+}
+
+//
+void PCA9635::writeMode(uint8_t reg, uint8_t value)
+{
+    if (reg == PCA9635_MODE1 || reg == PCA9635_MODE2)
+    {
+        writeReg(reg, value);
+    }
+}
+
+//
+uint8_t PCA9635::readMode(uint8_t reg)
+{
+    if ((reg == PCA9635_MODE1) || (reg == PCA9635_MODE2))
+    {
+        uint8_t value = readReg(reg);
+        return value;
+    }
+    return PCA9635_ERROR;
+}
+
+//
+void PCA9635::setLedDriverMode(uint8_t channel, uint8_t mode)
+{
+    if (channel <= 15 && mode <= 3)
+    {
+        uint8_t reg = PCA9635_LEDOUT_BASE + (channel >> 2);
+        // some bit magic
+        uint8_t shift = (channel & 0x03) * 2;  // 0,2,4,6 places
+        uint8_t setmask = mode << shift;
+        uint8_t clrmask = ~(0x03 << shift);
+        uint8_t value = (readReg(reg) & clrmask) | setmask;
+        writeReg(reg, value);
+    }
+}
+
+//
+uint8_t PCA9635::getLedDriverMode(uint8_t channel)
+{
+    if (channel <= 15)
+    {
+        uint8_t reg = PCA9635_LEDOUT_BASE + (channel >> 2);
+        uint8_t shift = (channel & 0x03) * 2;  // 0,2,4,6 places
+        return (readReg(reg) >> shift ) & 0x03;
+    }
+    return PCA9635_ERROR;
+}
+
+//
+int PCA9635::lastError()
+{
+    int e = _error;
+    _error = 0;
+    return e;
+}
+
+void PCA9635::writeReg(uint8_t reg, uint8_t value)
+{
+    Wire.beginTransmission(_address);
+    Wire.write(reg);
+    Wire.write(value);
+    _error = Wire.endTransmission();
+}
+
+uint8_t PCA9635::readReg(uint8_t reg)
+{
+    Wire.beginTransmission(_address);
+    Wire.write(reg);
+    _error = Wire.endTransmission();
+    if (Wire.requestFrom(_address, (uint8_t)1) != 1)
+    {
+        _error = PCA9635_ERROR;
+        return 0;
+    }
+    _data = Wire.read();
+    return _data;
+}
+
+//
+// END OF FILE
+//
---
+++ libraries/PCA9635/PCA9635.h
@@ -0,0 +1,84 @@
+//
+//    FILE: PCA9635.H
+//  AUTHOR: Rob Tillaart
+//    DATE: 23-apr-2013
+// VERSION: 0.1.00
+// PURPOSE: I2C PCA9635 library for Arduino
+//     URL: https://github.com/RobTillaart/Arduino/tree/master/libraries
+//
+// HISTORY:
+// see PCA9635.cpp file
+//
+
+#ifndef _PCA9635_H
+#define _PCA9635_H
+
+#include "Arduino.h"
+
+#define PCA9635_LIB_VERSION "0.1.00 BETA"
+
+#define PCA9635_MODE1       0x00
+#define PCA9635_MODE2       0x01
+#define PCA9635_PWM(x)      (0x02+(x))
+
+#define PCA9635_GRPPWM      0x12
+#define PCA9635_GRPFREQ     0x13
+
+// check datasheet for details
+#define PCA9635_LEDOUT_BASE 0x14    // 0x14..0x17
+#define PCA9635_LEDOFF      0x00    // default @ startup
+#define PCA9635_LEDON       0x01
+#define PCA9635_LEDPWM      0x02
+#define PCA9635_LEDGRPPWM   0x03
+
+#define PCA9635_OK          0x00
+#define PCA9635_ERROR       0xFF
+
+// NOT IMPLEMENTED YET
+#define PCA9635_SUBADR(x)   (0x17+(x))  // x = 1..3
+#define PCA9635_ALLCALLADR  0x1B
+
+class PCA9635
+{
+public:
+    explicit PCA9635(const uint8_t deviceAddress);
+
+    void setLedDriverMode(uint8_t channel, uint8_t mode);
+    uint8_t getLedDriverMode(uint8_t channel);
+
+    // single PWM setting
+    void write1(uint8_t channel, uint8_t value);
+    // RGB setting
+    void write3(uint8_t channel, uint8_t R, uint8_t G, uint8_t B);
+    // generic workhorse
+    void writeN(uint8_t channel, uint8_t* arr, uint8_t count);
+
+    // reg = 1, 2  check datasheet for values
+    void writeMode(uint8_t reg, uint8_t value);
+    uint8_t readMode(uint8_t reg);
+
+    // TODO PWM also in %% ?
+    void setGroupPWM(uint8_t value) { writeReg(PCA9635_GRPPWM, value); }
+    uint8_t getGroupPWM() { return readReg(PCA9635_GRPPWM); }
+
+    // TODO set time in millisec and round to nearest value?
+    void setGroupFREQ(uint8_t value) { writeReg(PCA9635_GRPFREQ, value); }
+    uint8_t getGroupFREQ() { return readReg(PCA9635_GRPFREQ); }
+
+    int lastError();
+
+private:
+    // DIRECT CONTROL
+    void writeReg(uint8_t reg, uint8_t value);
+    uint8_t readReg(uint8_t reg);
+
+    uint8_t _address;
+    uint8_t _register;
+    uint8_t _data;
+    int     _error;
+};
+
+#endif
+//
+// END OF FILE
+//

3c96a500675181f8d70045e0a87a7fc4b343438e
rob tillaart <rob.tillaart@gmail.com>
2016-05-02 19:23:06 +0200

    + 0.1.07 manually merged enhancements Septillion

--- libraries/PCF8574/PCF8574.cpp
+++ libraries/PCF8574/PCF8574.cpp
@@ -2,11 +2,20 @@
 //    FILE: PCF8574.cpp
 //  AUTHOR: Rob Tillaart
 //    DATE: 02-febr-2013
-// VERSION: 0.1.05
+// VERSION: 0.1.07
 // PURPOSE: I2C PCF8574 library for Arduino
-//     URL:
+//     URL: http://forum.arduino.cc/index.php?topic=184800
 //
 // HISTORY:
+// 0.1.07 2016-05-02 (manually merged) Septillion
+//        added dataOut so a write() doesn't read first,
+//        possibly corrupting a input pin;
+//        fixed shift comment, should read 1..7;
+//        added begin() to be sure it's in a known state,
+//        states could be different if uC is reset and the PCF8574 isn't;
+//        added buttonRead() and buttonRead8()
+//        which only effect the output while reading
+// 0.1.06 (intermediate) added defined errors + refactor rotate
 // 0.1.05 2016-04-30 refactor, +toggleMask, +rotLeft, +rotRight
 // 0.1.04 2015-05-09 removed ambiguity in read8()
 // 0.1.03 2015-03-02 address int -> uint8_t
@@ -27,8 +36,11 @@ PCF8574::PCF8574(const uint8_t deviceAddress)
     _address = deviceAddress;
     Wire.begin();
     // TWBR = 12; // 400KHz
-    _data = 0;
-    _error = 0;
+
+    _dataIn = 0;
+    _dataOut = 0;
+
+    _error = PCF8574_OK;
 }
 
 // removed Wire.beginTransmission(addr);
@@ -40,103 +52,127 @@ uint8_t PCF8574::read8()
 {
     if (Wire.requestFrom(_address, (uint8_t)1) != 1)
     {
-        _error = 10;
-        return _data; // last value
+        _error = PCF8574_I2C_ERROR;
+        return _dataIn; // last value
     }
 #if (ARDUINO <  100)
-    _data = Wire.receive();
+    _dataIn = Wire.receive();
 #else
-    _data = Wire.read();
+    _dataIn = Wire.read();
 #endif
-    return _data;
+    return _dataIn;
 }
-
 
 void PCF8574::write8(const uint8_t value)
 {
-    _data = value;
+    _dataOut = value;
     Wire.beginTransmission(_address);
-    Wire.write(_data);
+    Wire.write(_dataOut);
     _error = Wire.endTransmission();
 }
 
-// pin should be 0..7
 uint8_t PCF8574::read(const uint8_t pin)
 {
+    if (pin > 7)
+    {
+        _error = PCF8574_PIN_ERROR;
+        return 0;
+    }
     PCF8574::read8();
-    return (_data & (1 << pin)) > 0;
+    return (_dataIn & (1 << pin)) > 0;
 }
 
-// pin should be 0..7
 void PCF8574::write(const uint8_t pin, const uint8_t value)
 {
-    PCF8574::read8();
+    if (pin > 7)
+    {
+        _error = PCF8574_PIN_ERROR;
+        return;
+    }
     if (value == LOW)
     {
-        _data &= ~(1 << pin);
+        _dataOut &= ~(1 << pin);
     }
     else
     {
-        _data |= (1 << pin);
+        _dataOut |= (1 << pin);
     }
-    PCF8574::write8(_data);
+    write8(_dataOut);
 }
 
-// pin should be 0..7
 void PCF8574::toggle(const uint8_t pin)
 {
-    if (pin > 7 ) return;
+    if (pin > 7)
+    {
+        _error = PCF8574_PIN_ERROR;
+        return;
+    }
     toggleMask(1 << pin);
 }
 
-// invert = toggleAll = toggleMask(0xFF)
 void PCF8574::toggleMask(const uint8_t mask)
 {
-    PCF8574::read8();
-    _data ^= mask;
-    PCF8574::write8(_data);
+    _dataOut ^= mask;
+    PCF8574::write8(_dataOut);
 }
 
-// n should be 0..7
 void PCF8574::shiftRight(const uint8_t n)
 {
-    if (n == 0 || n > 7 ) return;
-    PCF8574::read8();
-    _data >>= n;
-    PCF8574::write8(_data);
+    if (n == 0 || n > 7) return;
+    _dataOut >>= n;
+    PCF8574::write8(_dataOut);
 }
 
-// n should be 0..7
 void PCF8574::shiftLeft(const uint8_t n)
 {
     if (n == 0 || n > 7) return;
-    PCF8574::read8();
-    _data <<= n;
-    PCF8574::write8(_data);
+    _dataOut <<= n;
+    PCF8574::write8(_dataOut);
 }
 
 int PCF8574::lastError()
 {
     int e = _error;
-    _error = 0;
+    _error = PCF8574_OK;
     return e;
 }
 
-// n should be 0..7
 void PCF8574::rotateRight(const uint8_t n)
 {
-    if (n == 0 || n > 7 ) return;
-    PCF8574::read8();
-    _data = (_data >> n) | (_data << (8-n));
-    PCF8574::write8(_data);
+    uint8_t r = n & 7;
+    _dataOut = (_dataOut >> r) | (_dataOut << (8-r));
+    PCF8574::write8(_dataOut);
 }
 
-// n should be 0..7
 void PCF8574::rotateLeft(const uint8_t n)
 {
-    rotateRight(8-n);
+    rotateRight(8- (n & 7));
 }
 
+//added 0.1.07 Septillion
+uint8_t PCF8574::readButton8()
+{
+    uint8_t temp = _dataOut;
+    PCF8574::begin();
+    uint8_t rtn = PCF8574::read8();
+    PCF8574::write8(temp);
+    return rtn;
+}
+
+//added 0.1.07 Septillion
+uint8_t PCF8574::readButton(const uint8_t pin)
+{
+    if (pin > 7)
+    {
+        _error = PCF8574_PIN_ERROR;
+        return 0;
+    }
+    uint8_t temp = _dataOut;
+    PCF8574::write(pin, HIGH);
+    uint8_t rtn = PCF8574::read(pin);
+    PCF8574::write8(temp);
+    return rtn;
+}
 
 //
 // END OF FILE
--- libraries/PCF8574/PCF8574.h
+++ libraries/PCF8574/PCF8574.h
@@ -2,9 +2,9 @@
 //    FILE: PCF8574.H
 //  AUTHOR: Rob Tillaart
 //    DATE: 02-febr-2013
-// VERSION: 0.1.05
+// VERSION: 0.1.07
 // PURPOSE: I2C PCF8574 library for Arduino
-//     URL:
+//     URL: http://forum.arduino.cc/index.php?topic=184800
 //
 // HISTORY:
 // see PCF8574.cpp file
@@ -19,27 +19,37 @@
 #include "WProgram.h"
 #endif
 
-#define PCF8574_LIB_VERSION "0.1.05"
+#define PCF8574_LIB_VERSION "0.1.07"
+
+#define PCF8574_OK          0x00
+#define PCF8574_PIN_ERROR   0x81
+#define PCF8574_I2C_ERROR   0x82
+
 
 class PCF8574
 {
 public:
     explicit PCF8574(const uint8_t deviceAddress);
 
+    void begin();
+
     uint8_t read8();
     uint8_t read(uint8_t pin);
-
-    uint8_t value() const { return _data; };
+    uint8_t value() const { return _dataIn; };
 
     void write8(const uint8_t value);
     void write(const uint8_t pin, const uint8_t value);
+    uint8_t valueOut() const { return _dataOut; }
 
+    //added 0.1.07 Septillion
+    uint8_t readButton8();
+    uint8_t readButton(const uint8_t pin);
+
+    // rotate, shift, toggle expect all lines are output
     void toggle(const uint8_t pin);
-    void toggleMask(const uint8_t mask);
-
+    void toggleMask(const uint8_t mask);    // invertAll() = toggleMask(0xFF)
     void shiftRight(const uint8_t n=1);
     void shiftLeft(const uint8_t n=1);
-
     void rotateRight(const uint8_t n=1);
     void rotateLeft(const uint8_t n=1);
 
@@ -47,7 +57,8 @@ public:
 
 private:
     uint8_t _address;
-    uint8_t _data;
+    uint8_t _dataIn;
+    uint8_t _dataOut;
     int _error;
 };
 

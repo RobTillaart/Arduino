0a93ce6c27effea57d221bdfd4e15d767244ea7f
RobTillaart <rob.tillaart@gmail.com>
2017-07-16 08:04:20 +0200

    - 0.1.5 support > 256 buckets; robustness; check allocated data

---
+++ libraries/Histogram/examples/hist_test_graph/hist_test_graph.ino
@@ -0,0 +1,61 @@
+//
+//    FILE: hist_test_graph.ino
+//  AUTHOR: Rob Tillaart
+//    DATE: 2017-07-16
+//
+// PUPROSE: test histogram frequency
+//
+
+#include "histogram.h"
+
+// boundaries array does not need to be equally distributed.
+double bounds[] = { 0, 100, 200, 300, 325, 350, 375, 400, 500, 600, 700, 800, 900, 1000 };
+
+Histogram hist(14, bounds);
+
+uint32_t lastTime = 0;
+const uint32_t threshold = 1000;  // milliseconds, for updating display
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.print(__FILE__);
+  Serial.print("\nHistogram version: ");
+  Serial.println(HISTOGRAM_LIB_VERSION);
+
+  Serial.print("# buckets: ");
+  Serial.println(hist.size());
+  Serial.println();
+}
+
+void loop()
+{
+  // "generator" for histogram data
+  int x = analogRead(A0);
+  hist.add(x);
+
+  // update output 
+  uint32_t now = millis();
+  if (now - lastTime > threshold)
+  {
+    lastTime = now;
+
+    for (int i = 0; i < hist.size(); i++)
+    {
+      Serial.print(i);
+      Serial.print("\t");
+      Serial.print(hist.frequency(i), 2);
+      Serial.print("\t");
+
+      int n = hist.frequency(i) * 50;  // 0..50
+      for (int p = 0; p < n; p++);
+      {
+        Serial.print(']')
+      }
+      Serial.println();
+    }
+    if (hist.count() > 1000000UL) hist.clear();
+  }
+}
+
+
--- libraries/Histogram/histogram.h
+++ libraries/Histogram/histogram.h
@@ -3,7 +3,7 @@
 //
 //    FILE: Histogram.h
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.1.03
+// VERSION: 0.1.5
 // PURPOSE: Histogram library for Arduino
 //    DATE: 2012-11-10
 //
@@ -20,34 +20,36 @@
 #include "WProgram.h"
 #endif
 
-#define HISTOGRAM_LIB_VERSION "0.1.04"
+#define HISTOGRAM_LIB_VERSION "0.1.5"
 
 class Histogram
 {
 public:
-    Histogram(const uint8_t len, double *bounds);
+    Histogram(const int16_t len, double *bounds);
     ~Histogram();
 
-    void clear();
-    void add(const double val);
-    void sub(const double val);
+    void      clear();
+    void      add(const double val);
+    void      sub(const double val);
 
-    uint8_t size();
-    unsigned long count();
-    long bucket(const uint8_t idx);
+    // number of buckets
+    inline int16_t size() { return _len; };
+    // number of values added to all buckets
+    inline uint32_t count() { return _cnt; };
+    // number of values added to single bucket
+    int32_t   bucket(const int16_t idx);
 
-    double frequency(const uint8_t idx);
-    double PMF(const double val);
-    double CDF(const double val);
-    double VAL(const double prob);
-    uint8_t find(const double f);
-    // void strategy();
+    double    frequency(const int16_t idx);
+    double    PMF(const double val);
+    double    CDF(const double val);
+    double    VAL(const double prob);
+    int16_t   find(const double f);
 
 protected:
-    double * _bounds;
-    long * _data;
-    uint8_t _len;
-    unsigned long _cnt;
+    double *  _bounds;
+    int32_t * _data;
+    int16_t   _len;
+    uint32_t  _cnt;
 };
 
 #endif

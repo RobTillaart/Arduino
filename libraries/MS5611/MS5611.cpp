7b8798199aa71fd67399c8a4ec078fd06931d358
RobTillaart <rob.tillaart@gmail.com>
2017-07-26 23:51:37 +0200

    revert double to float (issue 33); added keywords.txt

--- libraries/MS5611/MS5611.cpp
+++ libraries/MS5611/MS5611.cpp
@@ -2,11 +2,13 @@
 //    FILE: MS5611.cpp
 //  AUTHOR: Rob Tillaart
 //          Erni - testing/fixes
-// VERSION: 0.1.05
+// VERSION: 0.1.7
 // PURPOSE: MS5611 Temperature & Humidity library for Arduino
 //     URL:
 //
 // HISTORY:
+// 0.1.7  revert double to float (issue 33)
+// 0.1.6  2015-07-12 refactor
 // 0.1.05 moved 6 float multiplies to init  [adds ~70 bytes !!!]
 //        moved the MS5611_LIB_VERSION to PROGMEM
 // 0.1.04 changed float to double (for platforms which support it)
@@ -39,77 +41,81 @@
 //
 MS5611::MS5611(uint8_t deviceAddress)
 {
-    _address = deviceAddress;
-    Wire.begin();
+  _address = deviceAddress;
+  Wire.begin();
+  _temperature = -999;
+  _pressure = -999;
+  _result = -999;
+  init();
 }
 
 void MS5611::init()
 {
-    reset();
+  reset();
 
-    C[0] = 1;
-    C[1] = 32768L;
-    C[2] = 65536L;
-    C[3] = 3.90625E-3;
-    C[4] = 7.8125E-3;
-    C[5] = 256;
-    C[6] = 1.1920928955E-7;
-    for (uint8_t reg = 0; reg < 7; reg++)
-    {
-        // C[0] not used; this way indices match datasheet.
-        // C[7] == CRC skipped.
-        C[reg] *= readProm(reg);
-    }
+  C[0] = 1;
+  C[1] = 32768L;
+  C[2] = 65536L;
+  C[3] = 3.90625E-3;
+  C[4] = 7.8125E-3;
+  C[5] = 256;
+  C[6] = 1.1920928955E-7;
+  for (uint8_t reg = 0; reg < 7; reg++)
+  {
+    // C[0] not used; this way indices match datasheet.
+    // C[7] == CRC skipped.
+    C[reg] *= readProm(reg);
+  }
 }
 
 int MS5611::read(uint8_t bits)
 {
-    // VARIABLES NAMES BASED ON DATASHEET
-    convert(0x40, bits);
-    if (_result) return _result;
-    uint32_t D1 = readADC();
-    if (_result) return _result;
+  // VARIABLES NAMES BASED ON DATASHEET
+  convert(0x40, bits);
+  if (_result) return _result;
+  uint32_t D1 = readADC();
+  if (_result) return _result;
 
-    convert(0x50, bits);
-    if (_result) return _result;
-    uint32_t D2 = readADC();
-    if (_result) return _result;
+  convert(0x50, bits);
+  if (_result) return _result;
+  uint32_t D2 = readADC();
+  if (_result) return _result;
 
-    // TODO the multiplications of these constants can be done in init()
-    // but first they need to be verified.
-    
-    // TEMP & PRESS MATH - PAGE 7/20
-    double dT = D2 - C[5];
-    _temperature = 2000 + dT * C[6];
+  // TODO the multiplications of these constants can be done in init()
+  // but first they need to be verified.
 
-    double offset =  C[2] + dT * C[4];
-    double sens = C[1] + dT * C[3];
+  // TEMP & PRESS MATH - PAGE 7/20
+  float dT = D2 - C[5];
+  _temperature = 2000 + dT * C[6];
 
-    // SECOND ORDER COMPENSATION - PAGE 8/20
-    // COMMENT OUT < 20 CORRECTION IF NOT NEEDED
-    if (_temperature < 20)
+  float offset =  C[2] + dT * C[4];
+  float sens = C[1] + dT * C[3];
+
+  // SECOND ORDER COMPENSATION - PAGE 8/20
+  // COMMENT OUT < 20 CORRECTION IF NOT NEEDED
+  if (_temperature < 20)
+  {
+    float T2 = dT * dT * 4.6566128731E-10;
+    float t = _temperature - 2000;
+    float offset2 = 2.5 * t * t;
+    float sens2 = 1.25 * t * t * t;
+    // COMMENT OUT < -15 CORRECTION IF NOT NEEDED
+    if (_temperature < -15)
     {
-        double T2 = dT * dT * 4.6566128731E-10;
-        double t = _temperature - 2000;
-        double offset2 = 2.5 * t * t;
-        double sens2 = 1.25 * t * t * t;
-        // COMMENT OUT < -15 CORRECTION IF NOT NEEDED
-        if (_temperature < -15)
-        {
-            t = _temperature + 1500;
-            t = t * t;
-            offset2 += 7 * t;
-            sens2 += 5.5 * t;
-        }
-        _temperature -= T2;
-        offset -= offset2;
-        sens -= sens2;
+      t = _temperature + 1500;
+      t = t * t;
+      offset2 += 7 * t;
+      sens2 += 5.5 * t;
     }
-    // END SECOND ORDER COMPENSATION
+    _temperature -= T2;
+    offset -= offset2;
+    sens -= sens2;
+  }
+  // END SECOND ORDER COMPENSATION
 
-    _pressure = (D1 * sens * 4.76837158205E-7 - offset) * 3.051757813E-5;
+  _pressure = (D1 * sens * 4.76837158205E-7 - offset) * 3.051757813E-5;
 
-    return 0;
+  return 0;
 }
 
 
@@ -119,62 +125,62 @@ int MS5611::read(uint8_t bits)
 //
 void MS5611::reset()
 {
-    command(0x1E);
-    delay(3);
+  command(0x1E);
+  delay(3);
 }
 
-void MS5611::convert(uint8_t addr, uint8_t bits)
+void MS5611::convert(const uint8_t addr, uint8_t bits)
 {
-    uint8_t del[5] = {1,2,3,5,10};
-    bits = constrain(bits, 8, 12);
-    uint8_t offset = (bits - 8) * 2;
-    command(addr + offset);
-    delay(del[offset/2]);
+  uint8_t del[5] = {1, 2, 3, 5, 10};
+
+  bits = constrain(bits, 8, 12);
+  uint8_t offset = (bits - 8) * 2;
+  command(addr + offset);
+  delay(del[offset/2]);
 }
 
 uint16_t MS5611::readProm(uint8_t reg)
 {
-    reg = constrain(reg, 0, 7);
-    uint8_t offset = reg * 2;
-    command(0xA0 + offset);
-    if (_result == 0)
+  reg = min(reg, 7);      // constrain(reg, 0, 7) but reg is an uint so...
+  uint8_t offset = reg * 2;
+  command(0xA0 + offset);
+  if (_result == 0)
+  {
+    int nr = Wire.requestFrom(_address, (uint8_t)2);
+    if (nr >= 2)
     {
-        int nr = Wire.requestFrom(_address, (uint8_t)2);
-        if (nr >= 2)
-        {
-            uint16_t val = Wire.read() * 256;
-            val += Wire.read();
-            return val;
-        }
-        return 0;
+      uint16_t val = Wire.read() * 256;
+      val += Wire.read();
+      return val;
     }
     return 0;
+  }
+  return 0;
 }
 
 uint32_t MS5611::readADC()
 {
-    command(0x00);
-    if (_result == 0)
+  command(0x00);
+  if (_result == 0)
+  {
+    int nr = Wire.requestFrom(_address, (uint8_t)3);
+    if (nr >= 3)
     {
-        int nr = Wire.requestFrom(_address, (uint8_t)3);
-        if (nr >= 3)
-        {
-            uint32_t val = Wire.read() * 65536UL;
-            val += Wire.read() * 256UL;
-            val += Wire.read();
-            return val;
-        }
-        return 0UL;
+      uint32_t val = Wire.read() * 65536UL;
+      val += Wire.read() * 256UL;
+      val += Wire.read();
+      return val;
     }
     return 0UL;
+  }
+  return 0UL;
 }
 
-void MS5611::command(uint8_t command)
+void MS5611::command(const uint8_t command)
 {
-    Wire.beginTransmission(_address);
-    Wire.write(command);
-    _result = Wire.endTransmission();
+  Wire.beginTransmission(_address);
+  Wire.write(command);
+  _result = Wire.endTransmission();
 }
-//
+
 // END OF FILE
-//
--- libraries/MS5611/MS5611.h
+++ libraries/MS5611/MS5611.h
@@ -2,7 +2,7 @@
 //    FILE: MS5611.h
 //  AUTHOR: Rob Tillaart
 //          Erni - testing/fixes
-// VERSION: 0.1.05
+// VERSION: 0.1.7
 // PURPOSE: MS5611 Temperature & Pressure library for Arduino
 //     URL:
 //
@@ -19,35 +19,34 @@
 #include <Arduino.h>
 #endif
 
-#define MS5611_LIB_VERSION (F("0.1.05"))
+#define MS5611_LIB_VERSION (F("0.1.7"))
 
 #define MS5611_READ_OK  0
 
 class MS5611
 {
 public:
-    MS5611(uint8_t deviceAddress);
+  explicit MS5611(uint8_t deviceAddress);
 
-    void init();
-    int read(uint8_t bits = 8);
-    inline int32_t getTemperature() { return _temperature; };
-    inline int32_t getPressure() { return _pressure; };
-    inline int getLastResult()  { return _result; };
+  void           init();
+  int            read(uint8_t bits = 8);
+  inline int32_t getTemperature() const { return _temperature; };
+  inline int32_t getPressure() const { return _pressure; };
+  inline int     getLastResult() const { return _result; };
 
 private:
-    void reset();
-    void convert(uint8_t addr, uint8_t bits);
-    uint32_t readADC();
-    uint16_t readProm(uint8_t reg);
-    void command(const uint8_t command);
+  void     reset();
+  void     convert(const uint8_t addr, uint8_t bits);
+  uint32_t readADC();
+  uint16_t readProm(uint8_t reg);
+  void     command(const uint8_t command);
 
-    uint8_t _address;
-    int32_t _temperature;
-    int32_t _pressure;
-    int _result;
-    double C[7];
+  uint8_t  _address;
+  int32_t  _temperature;
+  int32_t  _pressure;
+  int      _result;
+  float    C[7];
 };
 #endif
-//
+
 // END OF FILE
-//
